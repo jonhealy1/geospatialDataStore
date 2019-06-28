2082d39d679af7e65ec22ffe8350857fb47c9f21
Jamie Hale <jhale@uvic.ca>
2019-06-27 20:14:24 -0700

    initial map

--- web_server/website/app.js
+++ web_server/website/app.js
@@ -1,1 +1,9 @@
-document.getElementById("text").innerHTML = "Hello World<br /><p>Welcome to the geospatialDataStore</p>";
+
+var mymap = L.map('mapid').setView([51.505, -0.09], 3);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1Ijoib290aGViYWxsc29vIiwiYSI6ImNqeGZpbGRheDA1c3Uzc284Y20xMGh3eWoifQ.5BZj7OvzM6cn6whqRl37XQ'
+}).addTo(mymap);
