5bfc44caaa8ded3968cb2ce1af617d863758653a
Jamie Hale <jamiehalebc@gmail.com>
2019-07-29 23:06:19 -0700

    front-end update -> Leaflet added

--- web_server/website/js/iframe.js
+++ web_server/website/js/iframe.js
@@ -1,6 +1,8 @@
 var global_current_lidar = ""
 
 function lidarLoad(item){
+    $('#full-screen-btn').attr('disabled', false)
+    $('#switcher-btn').attr('disabled', false)
     global_current_lidar = $(item).find("p").text()
     // set
     $('#viewFrame').attr('src', `http://localhost:8085/data/${global_current_lidar}.html`)
@@ -12,12 +14,29 @@ function lidarLoad(item){
     })
 }
 
-function fullscreenviewer() {
+function fullscreenviewer(b) {
     var win = window.open(`http://localhost:8085/data/${global_current_lidar}.html`, '_blank')
     win.focus();
 }
-    
+  
+function switcher(b){
+    console.log($(b).text())
+    var this_text = $(b).text()
+    if(this_text == 'Lidar'){
+        $('#map').hide()
+        $('#viewFrame').show()
+        $(b).text(function(){
+            return 'Map'
+        })
+    } else if (this_text == 'Map'){
+        $('#viewFrame').hide()
+        $('#map').show()
+        $(b).text(function(){
+            return 'Lidar'
+        })
+    }
+}
 
-$( window ).unload(function() {
+$(window).on("unload", function() {
     return $('#viewFrame').removeData('src');
   });
