69dffa737ff662236d3bf0806baa4633883fbedc
Jamie Hale <jamiehalebc@gmail.com>
2019-07-29 18:06:31 -0700

    UI update/full screen viewer/dynamic title

--- web_server/website/js/iframe.js
+++ web_server/website/js/iframe.js
@@ -1,8 +1,23 @@
-function lidarLoad(item){
-    var lid = $(item).find("p").text()
-    console.log($(item).find("p").text())
-    $('#viewFrame').attr('src', `http://localhost:8085/data/${lid}.html`)
-    $( '#viewFrame' ).attr( 'src', function ( i, val ) { return val; });
+var global_current_lidar = ""
 
+function lidarLoad(item){
+    global_current_lidar = $(item).find("p").text()
+    // set
+    $('#viewFrame').attr('src', `http://localhost:8085/data/${global_current_lidar}.html`)
+    //load
+    $( '#viewFrame' ).attr( 'src', function ( i, val ) { return val; });
+    //caption
+    $('#viewer-title').text(function(){
+        return `Viewing → ${global_current_lidar}` 
+    })
+}
+
+function fullscreenviewer() {
+    var win = window.open(`http://localhost:8085/data/${global_current_lidar}.html`, '_blank')
+    win.focus();
 }
     
+
+$( window ).unload(function() {
+    return $('#viewFrame').removeData('src');
+  });
